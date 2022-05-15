adb9d6e768ce412ace427be35a639ba442d1331f
jay <contact.jays.space@gmail.com>
2022-05-15 22:09:38 +0200

    Created PostUploadScreen and setup permission requests to the device

--- src/screens/PostUploadScreen/PostUploadScreen.tsx
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+import {Camera} from 'expo-camera';
 
 const PostUploadScreen = () => {
+  const [hasPermissions, setHasPermissions] = useState<boolean | null>(null);
+
+  //* Request permissions from user on component render.
+  useEffect(() => {
+    const getPermission = async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const micPermission = await Camera.requestMicrophonePermissionsAsync();
+
+      setHasPermissions(
+        cameraPermission.status === 'granted' &&
+          micPermission.status === 'granted',
+      );
+    };
+
+    getPermission();
+  }, []);
+
+  //* if application is awaiting response, return loading
+  if (hasPermissions === null) {
+    return <Text>Loading...</Text>;
+  }
+
+  //* if application is denied either camera or mic permissions, deny access to camera features
+  if (hasPermissions === false) {
+    return <Text>No Access to the camera</Text>;
+  }
+
+  //* if application is permitted both camera or mic permissions, allow access to camera features
   return (
-    <View>
+    <SafeAreaView>
       <Text>PostUploadScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
